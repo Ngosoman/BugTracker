0377d1dea327407ace97c9c5bdebccb5b3ab86e9
Tom Mwambingu <[tomchom30@gmail.com]>
2025-08-20 12:58:12 +0300

    Implemented Authentiation under views in accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    
+class CustomLogoutView(LogoutView):
+    next_page = 'home'
+    
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('login')
