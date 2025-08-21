31f4736c6026540dfb80a62421f908ac26237a36
Tom Mwambingu <[tomchom30@gmail.com]>
2025-08-21 20:37:54 +0300

    Updated URLS in issues

--- issues/urls.py
+++ issues/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('projects/', views.project_list, name='project_list'),
     path('projects/create/', views.project_create, name='project_create'),
     path('issues/', views.issue_list, name='issue_list'),
